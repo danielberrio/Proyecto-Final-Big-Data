0550b55e0c4dc317093278e7e1f9b39256e15654
Daniel Eduardo Berrío Beltrán <45372077+danielberrio@users.noreply.github.com>
2020-12-03 20:37:33 -0500

    Add files via upload

---
+++ credentials.py
@@ -0,0 +1,4 @@
+API_key = 'GQOs3O4z6LJaSRB5HUC699q5A'
+API_secret_key = 'JMZ0anpTgQdrLbTptt5WR9glufIomX7GwfW2CKcJpPS2FdRXWc'
+Access_token = '1154185151678689281-Cc7dHfFQGeZ0H0gfyOfUvIp0H0ojua'
+Access_token_secret = 'LYENu5DgZHH0kBnC3EF7r3dgZBUGkUnhbxkWTuUq0YqdP'
